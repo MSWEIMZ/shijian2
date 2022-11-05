596a9a02cbb9fba75cb2617cd6dfeff50db4c6bf
root <99701537@qq.com>
2022-11-05 14:46:11 +0800

    Initial commit

---
+++ src/test/java/Hello.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class Hello {
+    @Test
+    public void Hello(){
+        System.out.println("hello world！");
+    }
+
+    @Test
+    public void test(){
+        System.out.println("这是一个测试！");
+    }
+}
